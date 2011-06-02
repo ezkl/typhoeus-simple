e05baf87d7d4b439d1575e79d3fbcb2a3cb4edfe
Ezekiel Templin <ezkl@me.com>
2011-06-01 20:24:30 -0400

    Done, I think.

--- lib/typhoeus/simple.rb
+++ lib/typhoeus/simple.rb
@@ -1,1 +1,14 @@
+require 'typhoeus'
+require 'nokogiri'
+
 require 'typhoeus/simple/version'
+
+module Kernel
+  def get uri, params = {}
+    Typhoeus::Request.get(uri, params)
+  end unless method_defined?(:get)
+  
+  def get_parsed uri, params = {}, encoding = "UTF-8"
+    Nokogiri::HTML(get(uri, params).body, uri, encoding)
+  end unless method_defined?(:get_parsed)
+end
--- spec/simple_spec.rb
+++ spec/simple_spec.rb
@@ -5,4 +5,10 @@ describe Typhoeus::Simple do
   it "should have a VERSION constant" do
     subject.const_get('VERSION').should_not be_empty
   end
+  
+  it "should add get and get_parsed methods to Kernel" do
+    Kernel.method_defined?(:get).should be_true
+    Kernel.method_defined?(:get_parsed).should be_true
+  end
+  
 end
